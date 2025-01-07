b1faa88c0c02fb591252d65f088ed1c7956ab865
blob7 <128263564+blob7@users.noreply.github.com>
2025-01-07 04:25:40 -0500

    Create character_images.py

---
+++ src/app/data/character_images.py
@@ -0,0 +1,15 @@
+character_images = {
+    "Daemon": "",
+    "Gizmo": "",
+    "Maeve": "",
+    "Nidhoggr": "",
+    "Cass": "",
+    "Azreal": "",
+    "Kulev": "",
+    "Miko": "",
+    "ZeroCool": "",
+    "Buttercup": "",
+    "El Bastardo": "",
+    "Makutu": "",
+    "Mekko": ""
+}
